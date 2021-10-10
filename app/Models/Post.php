48043a66ea3c3184077ca9b038da936a551632a0
YukiYouda <youda0518@yahoo.co.jp>
2021-10-11 08:43:08 +0900

    記事登録機能作成前の準備まで完了

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -18,4 +18,9 @@ class Post extends Model
     {
         return $this->belongsTo(Category::class);
     }
+
+    public function likes()
+    {
+        return $this->hasMany(Like::class);
+    }
 }
